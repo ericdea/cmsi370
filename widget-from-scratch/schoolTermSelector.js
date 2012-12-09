7f27f987f4d2e6757eff8bc19a7a3bd2734c77aa
Eric Dea <ericdea@eric-deas-macbook-pro.local>
2012-12-09 13:47:31 -0800

    using .html() instead of .append() so it replaces what is currently there.

--- widget-from-scratch/schoolTermSelector.js
+++ widget-from-scratch/schoolTermSelector.js
@@ -1,8 +1,8 @@
 $(function() {
     $( "#previous" ).button().click(function( event ) {
-        $(".school-term-selector").append("<p>Test1</p>");
+        $(".school-term-selector").html("<p>Test1</p>");
     });
     $( "#next" ).button().click(function( event ) {
-        $(".school-term-selector").append("<p>Test2</p>");
+        $(".school-term-selector").html("<p>Test2</p>");
     });
 });
